828df37f9c06bf5d2de75ca170ce2c2275241f02
awarmisland <awarmisland@163.com>
2018-09-06 16:41:29 +0800

    插入图片 适配

--- app/src/main/java/com/awarmisland/android/richedittext/activity/MainActivity.java
+++ app/src/main/java/com/awarmisland/android/richedittext/activity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.awarmisland.android.richedittext.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Spanned;
@@ -28,6 +32,11 @@ import butterknife.OnClick;
  */
 public class MainActivity extends AppCompatActivity implements FontStylePanel.OnFontPanelListener
 ,RichEditText.OnSelectChangeListener {
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
     @BindView(R.id.richEditText)
     RichEditText richEditText;
     @BindView(R.id.fontStylePanel)
@@ -36,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements FontStylePanel.On
     Button btn_back;
     @BindView(R.id.btn_right)
     Button btn_right;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,6 +87,18 @@ public class MainActivity extends AppCompatActivity implements FontStylePanel.On
 
     @Override
     public void insertImg() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+            } else {
+                pickPicture();
+            }
+        } else {
+            pickPicture();
+        }
+    }
+    private void pickPicture(){
         Intent getAlbum = new Intent(Intent.ACTION_GET_CONTENT);
         getAlbum.setType("image/*");
         startActivityForResult(getAlbum, 0);
@@ -94,12 +116,12 @@ public class MainActivity extends AppCompatActivity implements FontStylePanel.On
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 0) {
             Uri originalUri = data.getData(); // 获得图片的uri
             String path = Utils.getRealPathFromUri(this,originalUri);
             richEditText.setImg(path);
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @OnClick(R.id.btn_right)
@@ -126,5 +148,9 @@ public class MainActivity extends AppCompatActivity implements FontStylePanel.On
      */
     @Override
     public void onSelect(int start, int end) {
+    }
+
+    private void checkPermission() {
+
     }
 }
